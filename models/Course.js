1ce087db665f04182d58c3b1ac57f910add2d741
KeiKei <hashimotokeigo23@gmail.com>
2025-12-12 19:57:33 +0900

    授業種類に表示を変更しました

--- models/Course.js
+++ models/Course.js
@@ -19,7 +19,7 @@ const courseSchema = new mongoose.Schema({
   required: true,
   enum: ['通常', '集中講義'],
   default: '通常'
-  }, // 開講パターン
+  }, // 授業種類
 
   // semester: { type: String, required: false }, // 開講時期
   credits: { type: Number, default: 1 },  
